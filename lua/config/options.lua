111cbfb56b650310a37ce282702adc980da63c61
Brendan Noble <brendann755@gmail.com>
2025-03-31 12:08:13 -0600

    Use Powershell in Windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,11 @@
 -- Add any additional options here
 vim.opt.guifont = "CaskaydiaCove Nerd Font Mono:h11"
 vim.g.autoformat = false
+
+-- Set PowerShell on Windows
+if vim.fn.has("win32") == 1 then
+  vim.opt.shell = "pwsh"
+  vim.opt.shellcmdflag = "-NoLogo -Command"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
