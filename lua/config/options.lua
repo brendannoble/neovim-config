7f42de66ef5f5e80a947c606e5f80f2dca865490
Brendan Noble <brendann755@gmail.com>
2025-12-05 12:59:00 -0700

    Set to absolute line count

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,7 @@ vim.opt.guifont = "CaskaydiaCove Nerd Font Mono:h11"
 vim.g.autoformat = false
 
 vim.diagnostic.enable(false)
+vim.opt.relativenumber = false;
 
 -- Set PowerShell on Windows
 if vim.fn.has("win32") == 1 then
