867ba2fe5b4eec765c00025d505621089088b789
Brendan Noble <brendann755@gmail.com>
2026-04-03 20:30:01 -0600

    feat: Add new neo-tree plugin

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      sources = { "filesystem", "buffers", "git_status" },
+      source_selector = {
+        winbar = true,
+        statusline = false,
+        sources = {
+          { source = "filesystem", display_name = "  File" },
+          { source = "buffers", display_name = "  Bufs" },
+          { source = "git_status", display_name = " 󰊢 Git" },
+        },
+      },
+      window = {
+        position = "left",
+        width = 48,
+      },
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+        },
+        use_libuv_file_watcher = true,
+      },
+    },
+  },
+}
